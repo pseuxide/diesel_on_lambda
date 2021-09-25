72d921e4686e69fec538f06fd89835d811047f68
s3pt3mb3r <s3pt3mb3r@protonmail.com>
2021-09-25 14:13:34 +0900

    hide token

--- src/lib.rs
+++ src/lib.rs
@@ -13,7 +13,7 @@ pub fn establish_connection() -> MysqlConnection {
     dotenv().ok();
 
     //env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-    let database_url = "mysql://s3pt3mb3r:Am4ne5262521@test-db.cef4eexsaboa.ap-northeast-1.rds.amazonaws.com:3306/user_info".to_string();
+    let database_url = "".to_string();
     MysqlConnection::establish(&database_url)
         .unwrap_or_else(|_| panic!("Error connecting to {}", database_url))
 }
